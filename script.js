c3a65a4efc75ef83e4bf1f306a010cfcc833193e
Aleksei Sanatov <alexsanatov@gmail.com>
2024-05-12 11:26:10 +0300

    added countdown

---
+++ script.js
@@ -0,0 +1,48 @@
+const deadline = "July 27 2024";
+
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+function initializeClock(endtime) {
+  const clock = document.querySelector(".main-left__guests-countdown");
+  const daysSpan = clock.querySelector(".days");
+  const hoursSpan = clock.querySelector(".hours");
+  const minutesSpan = clock.querySelector(".minutes");
+  const secondsSpan = clock.querySelector(".seconds");
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+    minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializeClock(deadline);
+
+  updateClock(); // run function once at first to avoid delay
+  var timeinterval = setInterval(updateClock, 1000);
+});
